7c1f4a149b0c940d3ab2fe1e5b61a781464caf8a
atrunand <aticha.ruengwattana@mail.kmutt.ac.th>
2021-09-10 09:32:58 +0700

    10-9-first

--- node-backend/index.js
+++ node-backend/index.js
@@ -26,6 +26,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(cors());
 
+// กำหนดการเชื่อมต่อกับ database mongoDB
 // Static directory path
 app.use(express.static(path.join(__dirname, 'dist/'))) // __dirname จะไปหาที่อยู่ของโปรเจคเรา เพื่อไม่ให้มีปัญหาในการเปิดเว็บที่อื่น แล้วไปแสดงผลที่ '/dist'
 // Base route
